1e286193d02de971046d8ed88b1bf362568ec6cd
Chris Whatley <chriswhatley81@hotmail.com>
2020-04-13 15:03:12 +0100

    Config updated and tested

--- config.php
+++ config.php
@@ -3,6 +3,7 @@
 return [
     'production' => false,
     'baseUrl' => '',
-    'siteTitle' => 'Jigsaw + Tailwind CSS Starter Kit',
+    'siteTitle' => "Tomorrow's Warehouse",
+    'siteDescription' => "Tomorrow's Warehouse is a one-day, free-to attend conference tackling operational challenges in warehousing and logistics at a time of unprecedented transformation.",
     'collections' => [],
 ];
