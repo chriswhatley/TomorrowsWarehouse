604f29dc744b27c50030edf9beb1f0cc015948f3
Chris Whatley <chriswhatley81@hotmail.com>
2020-05-26 16:50:58 +0100

    Production baseUrl updated to Netlify domain

--- config.production.php
+++ config.production.php
@@ -2,5 +2,5 @@
 
 return [
     'production' => true,
-    'baseUrl' => 'http://archives.westernbusinesspublishing.co.uk/misc/tw-staging/',
+    'baseUrl' => 'https://trusting-torvalds-0d8c79.netlify.app/',
 ];
